1c40942dc64316fdab00e8895e2d7dc7fdecab0d
Christopher Houghton <ChrisHoughton@ChristopherMBP.fios-router.home>
2022-01-18 17:19:38 -0500

    search fror artist tracks

--- server.js
+++ server.js
@@ -4,38 +4,17 @@ const app = express();
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const dotenv = require('dotenv')
+let spotifyApi;
 
 dotenv.config()
 
 app.use(cors())
    .use(bodyParser.json())
 
-app.post('/refresh', (req, res) => {
-  const refreshToken = req.body.refresh_token
-  const spotifyApi = new SpotifyWebApi({
-    redirectUri: 'http://localhost:3000/',
-    clientId: process.env.REACT_APP_CLIENT_ID,
-    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-    refreshToken
-  })
-  
-  spotifyApi
-    .refreshAccessToken()
-    .then(data => {
-      res.json({
-        expiresIn: data.body.expires_in,
-        accessToken: data.body.access_token
-      })
-    })
-    .catch(err => {
-      console.log(err)
-      res.sendStatus(400)
-    })
-})
-
 app.post('/login', (req, res) => {
   const code = req.body.code
-  const spotifyApi = new SpotifyWebApi({
+  console.log(code)
+  spotifyApi = new SpotifyWebApi({
     clientId: process.env.REACT_APP_CLIENT_ID,
     clientSecret: process.env.REACT_APP_CLIENT_SECRET,
     redirectUri: 'http://localhost:3000/'
@@ -56,6 +35,49 @@ app.post('/login', (req, res) => {
     })
 
 });
+
+app.post('/refresh', (req, res) => {
+  const refreshToken = req.body.refresh_token
+  spotifyApi = new SpotifyWebApi({
+    redirectUri: 'http://localhost:3000/',
+    clientId: process.env.REACT_APP_CLIENT_ID,
+    clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+    refreshToken
+  })
+  
+  spotifyApi
+    .refreshAccessToken()
+    .then(data => {
+      res.json({
+        expiresIn: data.body.expires_in,
+        accessToken: data.body.access_token
+      })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
+
+app.post('/search', (req, res) => {
+  const searchQuery = req.body.searchQuery
+  // const spotifyApi = new SpotifyWebApi({
+  //   redirectUri: 'http://localhost:3000/',
+  //   clientId: process.env.REACT_APP_CLIENT_ID,
+  //   clientSecret: process.env.REACT_APP_CLIENT_SECRET,
+  //   refreshToken
+  // })
+
+  spotifyApi
+    .searchTracks(searchQuery)
+    .then(response => {
+     res.json(response.body.tracks.items)
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(400)
+    })
+})
 
 
 app.listen(8008);
