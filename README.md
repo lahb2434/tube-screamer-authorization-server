37cc31198afb05cdcbc270c799027f6ffa6b9b74
Chris Houghton <chrishoughton2434@gmail.com>
2022-04-08 12:56:48 -0400

    minor fix

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tube-screamer-authorization-server
-Server dedicated to requesting authorization to access spotify 
+Server dedicated to requesting authorization and retreiving song information from Spotify Web API
+
+## Installation
+
+Install using [npm](https://docs.npmjs.com/downloading-and-installing-node-js-and-npm)
+
+```bash
+run $ npm install
+```
+
+
--- server.js
+++ server.js
@@ -21,13 +21,13 @@ server
 
 server.post('/login', (req, res) => {
   const code = req.body.code
+  
   spotifyApi
     .authorizationCodeGrant(code)
     .then( data => { 
       res.json({
         expiresIn: data.body.expires_in,
         accessToken: data.body.access_token,
-        // refreshToken: data.body.refresh_token
       })
       spotifyApi.setAccessToken(data.body.access_token);
       spotifyApi.setRefreshToken(data.body.refresh_token);
@@ -40,13 +40,6 @@ server.post('/login', (req, res) => {
 
 server.post('/refresh', (req, res) => {
   console.log(spotifyApi)
-  // const refreshToken = req.body.refresh_token
-  // spotifyApi = new SpotifyWebApi({
-  //   redirectUri: 'http://localhost:3000/',
-  //   clientId: process.env.REACT_APP_CLIENT_ID,
-  //   clientSecret: process.env.REACT_APP_CLIENT_SECRET,
-  //   refreshToken
-  // })
   
   spotifyApi
     .refreshAccessToken()
@@ -76,6 +69,7 @@ server.post('/search', (req, res) => {
       console.log(err)
       res.sendStatus(400)
     })
+})
 
 server.post('/requestAccess', (req, res) => {
   spotifyApi
@@ -84,7 +78,5 @@ server.post('/requestAccess', (req, res) => {
       console.log(response)
     })
 })
-})
-
 
 server.listen(8008);
